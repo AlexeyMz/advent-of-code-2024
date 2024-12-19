82ac9bec5c25ac37792aa4c091fdd12c54c7950d
Alexey Morozov <alexey.morozov@live.ru>
2024-12-19 23:11:42 +0300

    Add disassembler and solution ofr puzzle 17 (basic)

--- rust/src/puzzle17/main.rs
+++ rust/src/puzzle17/main.rs
@@ -1,24 +1,70 @@
 use core::get_data_path;
+use std::collections::HashSet;
 use regex::Regex;
-use std::fs::read_to_string;
+use std::env;
+use std::error::Error;
+use std::fs::{File, read_to_string};
+use std::io::{stdout, LineWriter, Write};
 
 fn main() {
-    use std::time::Instant;
-    let before = Instant::now();
-    basic();
-    println!("Elapsed time: {:.2?}", before.elapsed());
+    if env::args().nth(1).is_some_and(|arg| arg == "--disassemble") {
+        if let Some(program) = env::args().nth(2) {
+            match ComputerDescription::parse_program(&program) {
+                Ok(parsed) => {
+                    if let Err(err) = disassemble(&mut stdout(), &parsed) {
+                        eprintln!("Error disassembling the program:\n{}", err);
+                    }
+                }
+                Err(err) => {
+                    eprintln!("Error parsing the program:\n{}", err)
+                }
+            }
+        } else {
+            eprintln!("Missing program to disassemble");
+        }
+    } else {
+        use std::time::Instant;
+        let before = Instant::now();
+        basic();
+        println!("Elapsed time: {:.2?}", before.elapsed());
+    }
 }
 
 fn basic() {
-    let input = read_to_string(get_data_path("input/puzzle17_test.txt")).unwrap();
+    let input = read_to_string(get_data_path("input/puzzle17.txt")).unwrap();
     let description = ComputerDescription::parse(&input).unwrap();
+
+    {
+        let mut assembly_writer = LineWriter::new(
+            File::create(get_data_path("output/puzzle17_program.txt")).unwrap()
+        );
+        disassemble(&mut assembly_writer, &description.program).unwrap();
+    }
 
     let mut state = ComputerState::new();
     let mut output = Vec::new();
-    description.run(&mut state, |n| output.push(n));
+    description.run(&mut state, |n| output.push(n)).unwrap();
 
     let joined_output = output.iter().map(|n| n.to_string()).collect::<Vec<String>>().join(",");
     println!("Program output (basic): {}", joined_output);
+}
+
+fn disassemble<W: Write>(writer: &mut W, program: &Vec<u8>) -> Result<(), Box<dyn Error>> {
+    let instructions: Vec<_> = ComputerDescription::decode_program(program)?;
+    let mut labels: HashSet<usize> = HashSet::new();
+    for instruction in instructions.iter() {
+        if let Instruction(Opcode::Jnz, Operand::Literal(label)) = instruction {
+            labels.insert((*label).into());
+        }
+    }
+    for (i, instruction) in instructions.iter().enumerate() {
+        if labels.contains(&i) {
+            writeln!(writer, "#{}: {}", i, instruction)?;
+        } else {
+            writeln!(writer, "    {}", instruction)?;
+        }
+    }
+    return Ok(());
 }
 
 struct ComputerDescription {
@@ -46,12 +92,36 @@ impl ComputerDescription {
         let register_a = register_a.parse().map_err(|_| format!("Failed to parse register A: {}", register_a))?;
         let register_b = register_b.parse().map_err(|_| format!("Failed to parse register B: {}", register_b))?;
         let register_c = register_c.parse().map_err(|_| format!("Failed to parse register C: {}", register_c))?;
-        let program = program.split(",").flat_map(|v| v.parse::<u8>()).collect();
+        let program = Self::parse_program(program)?;
 
         return Ok(ComputerDescription { register_a, register_b, register_c, program });
     }
 
-    fn run(&self, state: &mut ComputerState, mut handle_output: impl FnMut(u64) -> ()) {
+    fn parse_program(program: &str) -> Result<Vec<u8>, String> {
+        program.split(",")
+            .enumerate()
+            .map(|(i, v)| v.parse::<u8>()
+                .map_err(|_| format!("Invalid instruction byte #{i}: {v}"))
+            )
+            .collect()
+    }
+
+    fn decode_program(program: &Vec<u8>) -> Result<Vec<Instruction>, String> {
+        let chunks = program.chunks_exact(2);
+        let decoded = chunks
+            .clone()
+            .enumerate()
+            .map(|(i, pair)| Instruction::decode(pair[0], pair[1])
+                .ok_or(format!("Invalid instruction #{i}: {},{}", pair[0], pair[1]))
+            )
+            .collect();
+        if chunks.remainder().len() > 0 {
+            return Err(format!("Unexpected trailing program bytes"));
+        }
+        return decoded;
+    }
+
+    fn run(&self, state: &mut ComputerState, mut handle_output: impl FnMut(u64) -> ()) -> Result<(), String> {
         state.register_a = self.register_a;
         state.register_b = self.register_b;
         state.register_c = self.register_c;
@@ -59,23 +129,23 @@ impl ComputerDescription {
 
         while state.instruction < self.program.len() {
             let index: usize = state.instruction;
-            let opcode_value = self.program[index];
-            let opcode: Opcode = opcode_value.try_into()
-                .expect(&format!("Unexpected opcode #{index} value: {opcode_value}"));
-            let operand = self.program[index + 1];
+            let raw_code = self.program[index];
+            let raw_operand = self.program[index + 1];
+            let Instruction(opcode, operand) = Instruction::decode(raw_code, raw_operand)
+                .ok_or(format!("Invalid instruction #{index}: {raw_code},{raw_operand}"))?;
             match opcode {
                 Opcode::Adv => {
-                    state.register_a >>= combo_operand(operand, state);
+                    state.register_a >>= state.read(&operand);
                 }
                 Opcode::Bxl => {
-                    state.register_b ^= literal_operand(operand);
+                    state.register_b ^= state.read(&operand);
                 }
                 Opcode::Bst => {
-                    state.register_b = combo_operand(operand, state) % 8;
+                    state.register_b = state.read(&operand) % 8;
                 }
                 Opcode::Jnz => {
                     if state.register_a != 0 {
-                        state.instruction = literal_operand(operand).try_into().unwrap();
+                        state.instruction = state.read(&operand).try_into().unwrap();
                         continue;
                     }
                 }
@@ -83,17 +153,18 @@ impl ComputerDescription {
                     state.register_b ^= state.register_c;
                 }
                 Opcode::Out => {
-                    handle_output(combo_operand(operand, state));
+                    handle_output(state.read(&operand) % 8);
                 }
                 Opcode::Bdv => {
-                    state.register_b = state.register_a >> combo_operand(operand, state);
+                    state.register_b = state.register_a >> state.read(&operand);
                 }
                 Opcode::Cdv => {
-                    state.register_c = state.register_a >> combo_operand(operand, state);
+                    state.register_c = state.register_a >> state.read(&operand);
                 }
             }
             state.instruction += 2;
         }
+        return Ok(());
     }
 }
 
@@ -113,7 +184,19 @@ impl ComputerState {
             instruction: 0,
         }
     }
+
+    fn read(&self, operand: &Operand) -> u64 {
+        match operand {
+            Operand::Literal(value) => (*value).into(),
+            Operand::Register(Register::A) => self.register_a,
+            Operand::Register(Register::B) => self.register_b,
+            Operand::Register(Register::C) => self.register_c,
+            Operand::Unused(value) => panic!("Cannot read unsed operand with value {value}")
+        }
+    }
 }
+
+struct Instruction(Opcode, Operand);
 
 enum Opcode {
     Adv = 0,
@@ -126,34 +209,88 @@ enum Opcode {
     Cdv = 7,
 }
 
-impl TryFrom<u8> for Opcode {
-    type Error = ();
+enum Operand {
+    Literal(u8),
+    Register(Register),
+    Unused(u8),
+}
 
-    fn try_from(value: u8) -> Result<Self, Self::Error> {
-        match value {
-            0 => Ok(Opcode::Adv),
-            1 => Ok(Opcode::Bxl),
-            2 => Ok(Opcode::Bst),
-            3 => Ok(Opcode::Jnz),
-            4 => Ok(Opcode::Bxc),
-            5 => Ok(Opcode::Out),
-            6 => Ok(Opcode::Bdv),
-            7 => Ok(Opcode::Cdv),
-            _ => Err(()),
+enum Register { A, B, C }
+
+impl Instruction {
+    fn decode(opcode: u8, operand: u8) -> Option<Instruction> {
+        match opcode {
+            0 => Some(Instruction(Opcode::Adv, Self::decode_combo(operand))),
+            1 => Some(Instruction(Opcode::Bxl, Self::decode_literal(operand))),
+            2 => Some(Instruction(Opcode::Bst, Self::decode_combo(operand))),
+            3 => Some(Instruction(Opcode::Jnz, Self::decode_literal(operand))),
+            4 => Some(Instruction(Opcode::Bxc, Self::decode_literal(operand))),
+            5 => Some(Instruction(Opcode::Out, Self::decode_combo(operand))),
+            6 => Some(Instruction(Opcode::Bdv, Self::decode_combo(operand))),
+            7 => Some(Instruction(Opcode::Cdv, Self::decode_combo(operand))),
+            _ => None,
+        }
+    }
+
+    fn decode_literal(operand: u8) -> Operand {
+        Operand::Literal(operand)
+    }
+
+    fn decode_combo(operand: u8) -> Operand {
+        match operand {
+            0..=3 => Operand::Literal(operand),
+            4 => Operand::Register(Register::A),
+            5 => Operand::Register(Register::B),
+            6 => Operand::Register(Register::C),
+            _ => Operand::Unused(operand),
         }
     }
 }
 
-fn literal_operand(value: u8) -> u64 {
-    return value.into();
+impl std::fmt::Display for Instruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.0 {
+            Opcode::Adv => {
+                write!(f, "adv A >> {} -> A", self.1)
+            }
+            Opcode::Bxl => {
+                write!(f, "bxl B ^ {} -> B", self.1)
+            }
+            Opcode::Bst => {
+                write!(f, "bst {} % 8 -> B", self.1)
+            }
+            Opcode::Jnz => {
+                write!(f, "jnz if A goto #{}", self.1)
+            }
+            Opcode::Bxc => {
+                write!(f, "bxc B ^ C -> B : {}", self.1)
+            }
+            Opcode::Out => {
+                write!(f, "out {}", self.1)
+            }
+            Opcode::Bdv => {
+                write!(f, "bdv A >> {} -> B", self.1)
+            }
+            Opcode::Cdv => {
+                write!(f, "cdv A >> {} -> C", self.1)
+            }
+        }
+    }
 }
 
-fn combo_operand(value: u8, state: &ComputerState) -> u64 {
-    match value {
-        0..=3 => value.into(),
-        4 => state.register_a,
-        5 => state.register_b,
-        6 => state.register_c,
-        _ => panic!("Unexpected combo operand {value}")
+impl std::fmt::Display for Operand {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Operand::Literal(value) => write!(f, "{value}"),
+            Operand::Register(register) => {
+                let register = match register {
+                    Register::A => "A",
+                    Register::B => "B",
+                    Register::C => "C",
+                };
+                write!(f, "{register}")
+            },
+            Operand::Unused(value) => write!(f, "unused({value})")
+        }
     }
 }
